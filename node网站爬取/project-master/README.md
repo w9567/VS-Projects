fdfb14c3a9ebb3fcf197af87dd447e6074d2acfe
w9567 <956792115@qq.com>
2022-02-27 22:33:59 +0800

    delete useless

--- node网站爬取/project-master/README.md
+++
@@ -1,8 +0,0 @@
-# SSH_Forum
-前台
-
-# SSH_FourmBack
-后台
-
-#project_01
- 基于node实现爬取网页图片:详细过程：https://www.cnblogs.com/jiguiyan/p/11212387.html
--- node网站爬取/project-master/project_01/config.js
+++
@@ -1,7 +0,0 @@
-const url='https://motorcycle.sh.cn/portal.php';//填写自己请求的具体的网址
-
-const path=require('path');
-const imgDir=path.join(__dirname,'img');
-
-module.exports.url=url;
-module.exports.imgDir=imgDir;
